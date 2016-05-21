d0316b43d803dc850ae7490905b7088aff555bdc
ichland <ichland@gmail.com>
2016-05-21 16:11:36 +0300

    generate random

--- python_training/gps/gps_generator/gps_generator.py
+++ python_training/gps/gps_generator/gps_generator.py
@@ -2,10 +2,28 @@
 """Generate file with GPS points
 
 """
+import random
+import time
+
+
+def get_random(max):
+    random.seed(time.time())
+    r = random.uniform(0, max)
+    return r
+
+
+def get_next_point(point, distance):
+    next_point = []
+
+    for i in point:
+        next_point.append(i + get_random(distance))
+    return next_point
 
 
 def generate():
-    print("hello world")
+    point = [1, 2, 3]
+    next_point = get_next_point(point, 5)
+    print(next_point)
     return
 
 
