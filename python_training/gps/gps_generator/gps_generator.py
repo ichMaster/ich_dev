78d401dc70d000e903e4a5c59a1a211a40e37322
ichland <ichland@gmail.com>
2016-05-21 16:22:10 +0300

    generate point on circle

--- python_training/gps/gps_generator/gps_generator.py
+++ python_training/gps/gps_generator/gps_generator.py
@@ -4,6 +4,7 @@
 """
 import random
 import time
+import math
 
 
 def get_random(max):
@@ -14,21 +15,25 @@ def get_random(max):
 
 def get_next_point(point, distance):
     next_point = []
-
-    for i in point:
-        next_point.append(i + get_random(distance))
+    ang = get_random(360)
+    lat = point[0] + distance * math.cos(ang)
+    lon = point[1] + distance * math.sin(ang)
+    next_point.append(lat)
+    next_point.append(lon)
+    next_point.append(point[2])
     return next_point
 
 
-def generate():
-    point = [1, 2, 3]
+def generate(start_point, step, amount):
+    point = [100, 100, 3]
     next_point = get_next_point(point, 5)
     print(next_point)
     return
 
 
 def main():
-    generate()
+    start_point = [100, 100, 1]
+    generate(start_point, 5, 10)
     return
 
 
